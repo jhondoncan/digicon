5b365c90167a39ac1c7e7884bd8f412aca3e6efc
Jhon Arlem Andrade <jhondoncan@outlook.com>
2022-10-08 17:41:21 -0500

    Se realizan ajustes en los titulos de los planes se pone en negrita, se agrega link a los botones cotizar plan en pagina plan Santa Maria e Index, se agrega whatsappmsg.js

---
+++ js/whatsappmsg.js
@@ -0,0 +1,67 @@
+/* Mensajes Planes Santa Maria Huila  */
+
+let number = "573153759631";
+let msg = "";
+let url = "https://api.whatsapp.com/send?phone=" + number + "&text=";
+
+function fibraOpSantaTreinta() {
+    msg = "¡Hola! Deseo información acerca del plan *30 Mbs Fibra Óptica residencial urbano Santa María*";
+    window.open(url + msg, '_blank');
+}
+
+function fibraOpSantaCuarenta() {
+    msg = "¡Hola! Deseo información acerca del plan *40 Mbs Fibra Óptica residencial urbano Santa María*";
+    window.open(url + msg, '_blank');
+}
+
+
+function fibraOpSantaCincuenta() {
+    msg = "¡Hola! Deseo información acerca del plan *50 Mbs Fibra Óptica residencial urbano Santa María*";
+    window.open(url + msg, '_blank');
+}
+
+
+function fibraOpSantaSesenta() {
+    msg = "¡Hola! Deseo información acerca del plan *60 Mbs Fibra Óptica residencial urbano Santa María*";
+    window.open(url + msg, '_blank');
+}
+
+/* Fibra optica rural Santa Maria */
+
+function fibraOpSantaRuDiez() {
+    msg = "¡Hola! Deseo información acerca del plan *10 Mbs Fibra Óptica residencial rural Santa María*";
+    window.open(url + msg, '_blank');
+}
+
+function fibraOpSantaRuQuince() {
+    msg = "¡Hola! Deseo información acerca del plan *15 Mbs Fibra Óptica residencial rural Santa María*";
+    window.open(url + msg, '_blank');
+}
+
+function fibraOpSantaRuVeinte() {
+    msg = "¡Hola! Deseo información acerca del plan *20 Mbs Fibra Óptica residencial rural Santa María*";
+    window.open(url + msg, '_blank');
+}
+
+function fibraOpSantaRuTreinta() {
+    msg = "¡Hola! Deseo información acerca del plan *30 Mbs Fibra Óptica residencial rural Santa María*";
+    window.open(url + msg, '_blank');
+}
+
+
+/* Radio enlace Santa Maria */
+
+function radioEnlaSantaSeis() {
+    msg = "¡Hola! Deseo información acerca del plan *6 Mbs Radio Enlace Santa María*";
+    window.open(url + msg, '_blank');
+}
+
+function radioEnlaSantaOcho() {
+    msg = "¡Hola! Deseo información acerca del plan *8 Mbs Radio Enlace Santa María*";
+    window.open(url + msg, '_blank');
+}
+
+function radioEnlaSantaDiez() {
+    msg = "¡Hola! Deseo información acerca del plan *10 Mbs Radio Enlace Santa María*";
+    window.open(url + msg, '_blank');
+}
