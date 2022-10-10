e2bc4ecd3bbfdf250b04e7ac229c2c6e90168ef1
Jhon Arlem Andrade <jhondoncan@outlook.com>
2022-10-10 10:34:43 -0500

    Se termina de configurar mensajes de whatsapp para los botones de cotizar planes Saldaña en index y planes-saldana

--- js/whatsappmsg.js
+++ js/whatsappmsg.js
@@ -65,3 +65,70 @@ function radioEnlaSantaDiez() {
     msg = "¡Hola! Deseo información acerca del plan *10 Mbs Radio Enlace Santa María*";
     window.open(url + msg, '_blank');
 }
+
+
+/* ------------------------ SALDAÑA -------------------- */
+
+/* Mensajes Planes Saldaña  */
+function fibraOpSaldanaVeinte() {
+    msg = "¡Hola! Deseo información acerca del plan *20 Mbs Fibra Óptica residencial urbano Saldaña*";
+    window.open(url + msg, '_blank');
+}
+
+function fibraOpSaldanaVeinticinco() {
+    msg = "¡Hola! Deseo información acerca del plan *25 Mbs Fibra Óptica residencial urbano Saldaña*";
+    window.open(url + msg, '_blank');
+}
+
+
+function fibraOpSaldanaTreinta() {
+    msg = "¡Hola! Deseo información acerca del plan *30 Mbs Fibra Óptica residencial urbano Saldaña*";
+    window.open(url + msg, '_blank');
+}
+
+function fibraOpSaldanaCuarenta() {
+    msg = "¡Hola! Deseo información acerca del plan *40 Mbs Fibra Óptica residencial urbano Saldana*";
+    window.open(url + msg, '_blank');
+}
+
+
+
+/* Fibra optica rural Saldaña */
+
+function fibraOpSaldanaRuQuince() {
+    msg = "¡Hola! Deseo información acerca del plan *15 Mbs Fibra Óptica residencial rural Saldaña*";
+    window.open(url + msg, '_blank');
+}
+
+function fibraOpSaldanaRuVeinte() {
+    msg = "¡Hola! Deseo información acerca del plan *20 Mbs Fibra Óptica residencial rural Saldaña*";
+    window.open(url + msg, '_blank');
+}
+
+function fibraOpSaldanaRuVeinticinco() {
+    msg = "¡Hola! Deseo información acerca del plan *25 Mbs Fibra Óptica residencial rural Saldaña*";
+    window.open(url + msg, '_blank');
+}
+
+function fibraOpSaldanaRuTreinta() {
+    msg = "¡Hola! Deseo información acerca del plan *30 Mbs Fibra Óptica residencial rural Saldaña*";
+    window.open(url + msg, '_blank');
+}
+
+
+/* Radio enlace Saldaña */
+
+function radioEnlaSaldanaSeis() {
+    msg = "¡Hola! Deseo información acerca del plan *6 Mbs Radio Enlace Saldana*";
+    window.open(url + msg, '_blank');
+}
+
+function radioEnlaSaldanaOcho() {
+    msg = "¡Hola! Deseo información acerca del plan *8 Mbs Radio Enlace Saldana*";
+    window.open(url + msg, '_blank');
+}
+
+function radioEnlaSaldanaDiez() {
+    msg = "¡Hola! Deseo información acerca del plan *10 Mbs Radio Enlace Saldana*";
+    window.open(url + msg, '_blank');
+}
