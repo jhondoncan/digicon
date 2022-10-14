621c7d9688c676eaaf50f75ec1faef6a2da41873
Jhon Arlem Andrade <jhondoncan@outlook.com>
2022-10-14 09:00:33 -0500

    Se optimiza codigo de envio de mensajes, se corrije ortografia en los mensajes de Saldaña

--- js/whatsappmsg.js
+++ js/whatsappmsg.js
@@ -1,70 +1,64 @@
 /* Mensajes Planes Santa Maria Huila  */
 
 let number = "573153759631";
-let msg = "";
+let msg = "¡Hola! Deseo información acerca del plan ";
 let url = "https://api.whatsapp.com/send?phone=" + number + "&text=";
 
 function fibraOpSantaTreinta() {
-    msg = "¡Hola! Deseo información acerca del plan *30 Mbs Fibra Óptica residencial urbano Santa María*";
+    msg += "*30 Mbs Fibra Óptica residencial urbano Santa María*";
     window.open(url + msg, '_blank');
 }
 
 function fibraOpSantaCuarenta() {
-    msg = "¡Hola! Deseo información acerca del plan *40 Mbs Fibra Óptica residencial urbano Santa María*";
+    msg += "*40 Mbs Fibra Óptica residencial urbano Santa María*";
     window.open(url + msg, '_blank');
 }
-
 
 function fibraOpSantaCincuenta() {
-    msg = "¡Hola! Deseo información acerca del plan *50 Mbs Fibra Óptica residencial urbano Santa María*";
+    msg += "*50 Mbs Fibra Óptica residencial urbano Santa María*";
     window.open(url + msg, '_blank');
 }
 
-
 function fibraOpSantaSesenta() {
-    msg = "¡Hola! Deseo información acerca del plan *60 Mbs Fibra Óptica residencial urbano Santa María*";
+    msg += "*60 Mbs Fibra Óptica residencial urbano Santa María*";
     window.open(url + msg, '_blank');
 }
 
 /* Fibra optica rural Santa Maria */
-
 function fibraOpSantaRuDiez() {
-    msg = "¡Hola! Deseo información acerca del plan *10 Mbs Fibra Óptica residencial rural Santa María*";
+    msg += "*10 Mbs Fibra Óptica residencial rural Santa María*";
     window.open(url + msg, '_blank');
 }
 
 function fibraOpSantaRuQuince() {
-    msg = "¡Hola! Deseo información acerca del plan *15 Mbs Fibra Óptica residencial rural Santa María*";
+    msg += "*15 Mbs Fibra Óptica residencial rural Santa María*";
     window.open(url + msg, '_blank');
 }
 
 function fibraOpSantaRuVeinte() {
-    msg = "¡Hola! Deseo información acerca del plan *20 Mbs Fibra Óptica residencial rural Santa María*";
+    msg += "*20 Mbs Fibra Óptica residencial rural Santa María*";
     window.open(url + msg, '_blank');
 }
 
 function fibraOpSantaRuTreinta() {
-    msg = "¡Hola! Deseo información acerca del plan *30 Mbs Fibra Óptica residencial rural Santa María*";
+    msg += "*30 Mbs Fibra Óptica residencial rural Santa María*";
     window.open(url + msg, '_blank');
 }
 
 
 /* Radio enlace Santa Maria */
-
-
 function radioEnlaSantaOcho() {
-    msg = "¡Hola! Deseo información acerca del plan *8 Mbs Radio Enlace Santa María*";
+    msg += "*8 Mbs Radio Enlace Santa María*";
     window.open(url + msg, '_blank');
 }
 
 function radioEnlaSantaDiez() {
-    msg = "¡Hola! Deseo información acerca del plan *10 Mbs Radio Enlace Santa María*";
+    msg += "*10 Mbs Radio Enlace Santa María*";
     window.open(url + msg, '_blank');
 }
 
-
 function radioEnlaSantaDoce() {
-    msg = "¡Hola! Deseo información acerca del plan *12 Mbs Radio Enlace Santa María*";
+    msg += "*12 Mbs Radio Enlace Santa María*";
     window.open(url + msg, '_blank');
 }
 
@@ -73,65 +67,61 @@ function radioEnlaSantaDoce() {
 
 /* Mensajes Planes Saldaña  */
 function fibraOpSaldanaVeinte() {
-    msg = "¡Hola! Deseo información acerca del plan *20 Mbs Fibra Óptica residencial urbano Saldaña*";
+    msg += "*20 Mbs Fibra Óptica residencial urbano Saldaña*";
     window.open(url + msg, '_blank');
 }
 
 function fibraOpSaldanaVeinticinco() {
-    msg = "¡Hola! Deseo información acerca del plan *25 Mbs Fibra Óptica residencial urbano Saldaña*";
+    msg += "*25 Mbs Fibra Óptica residencial urbano Saldaña*";
     window.open(url + msg, '_blank');
 }
 
-
 function fibraOpSaldanaTreinta() {
-    msg = "¡Hola! Deseo información acerca del plan *30 Mbs Fibra Óptica residencial urbano Saldaña*";
+    msg += "*30 Mbs Fibra Óptica residencial urbano Saldaña*";
     window.open(url + msg, '_blank');
 }
 
 function fibraOpSaldanaCuarenta() {
-    msg = "¡Hola! Deseo información acerca del plan *40 Mbs Fibra Óptica residencial urbano Saldana*";
+    msg += "*40 Mbs Fibra Óptica residencial urbano Saldana*";
     window.open(url + msg, '_blank');
 }
 
 
-
 /* Fibra optica rural Saldaña */
-
 function fibraOpSaldanaRuQuince() {
-    msg = "¡Hola! Deseo información acerca del plan *15 Mbs Fibra Óptica residencial rural Saldaña*";
+    msg += "*15 Mbs Fibra Óptica residencial rural Saldaña*";
     window.open(url + msg, '_blank');
 }
 
 function fibraOpSaldanaRuVeinte() {
-    msg = "¡Hola! Deseo información acerca del plan *20 Mbs Fibra Óptica residencial rural Saldaña*";
+    msg = "*20 Mbs Fibra Óptica residencial rural Saldaña*";
     window.open(url + msg, '_blank');
 }
 
 function fibraOpSaldanaRuVeinticinco() {
-    msg = "¡Hola! Deseo información acerca del plan *25 Mbs Fibra Óptica residencial rural Saldaña*";
+    msg = "*25 Mbs Fibra Óptica residencial rural Saldaña*";
     window.open(url + msg, '_blank');
 }
 
 function fibraOpSaldanaRuTreinta() {
-    msg = "¡Hola! Deseo información acerca del plan *30 Mbs Fibra Óptica residencial rural Saldaña*";
+    msg += "*30 Mbs Fibra Óptica residencial rural Saldaña*";
     window.open(url + msg, '_blank');
 }
 
 
 /* Radio enlace Saldaña */
-
 function radioEnlaSaldanaSeis() {
-    msg = "¡Hola! Deseo información acerca del plan *6 Mbs Radio Enlace Saldana*";
+    msg += "*6 Mbs Radio Enlace Saldaña*";
     window.open(url + msg, '_blank');
 }
 
 function radioEnlaSaldanaOcho() {
-    msg = "¡Hola! Deseo información acerca del plan *8 Mbs Radio Enlace Saldana*";
+    msg += "*8 Mbs Radio Enlace Saldaña*";
     window.open(url + msg, '_blank');
 }
 
 function radioEnlaSaldanaDiez() {
-    msg = "¡Hola! Deseo información acerca del plan *10 Mbs Radio Enlace Saldana*";
+    msg += "*10 Mbs Radio Enlace Saldaña*";
     window.open(url + msg, '_blank');
 }
 
